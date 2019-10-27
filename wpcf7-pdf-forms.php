20c188bee0167a5c9e4baa3910efe735c794d3ed
Alex <alex@maximum.guru>
2019-10-27 12:19:01 -0700

    Fixed an issue which caused the plugin to deactivate when updating from pre-1.2 versions to 1.2.0 and later versions due to main plugin php file rename

---
+++ wpcf7-pdf-forms.php
@@ -0,0 +1,9 @@
+<?php
+
+// this file is needed for smooth upgrades from pre-1.2 versions to 1.2.0 and later versions
+// it fixes a plugin deactivation on upgrade issue due to main plugin php file rename
+
+if( ! function_exists( 'activate_plugin' ) )
+	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+activate_plugin( plugin_basename( trailingslashit( dirname( __FILE__ ) ) . 'pdf-forms-for-contact-form-7.php' ) );
