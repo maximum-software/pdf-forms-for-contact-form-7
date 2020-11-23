a51d8f19effa7185fd8e9be22230d6fd1d2d76bd
Alex <alex@maximum.guru>
2020-11-23 15:49:59 -0800

    Added old browser compatibility

--- js/admin.js
+++ js/admin.js
@@ -219,12 +219,43 @@ jQuery(document).ready(function($) {
 	
 	var cf7FieldsCache = [];
 	
+	// Object assign polyfill, courtesy of https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+	if (typeof Object.assign !== 'function') {
+		// Must be writable: true, enumerable: false, configurable: true
+		Object.defineProperty(Object, "assign", {
+			value: function assign(target, varArgs) { // .length of function is 2
+				'use strict';
+				if (target === null || target === undefined) {
+					throw new TypeError('Cannot convert undefined or null to object');
+				}
+				
+				var to = Object(target);
+				
+				for (var index = 1; index < arguments.length; index++) {
+					var nextSource = arguments[index];
+					
+					if (nextSource !== null && nextSource !== undefined) {
+						for (var nextKey in nextSource) {
+							// Avoid bugs when hasOwnProperty is shadowed
+							if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+							  to[nextKey] = nextSource[nextKey];
+							}
+						}
+					}
+				}
+				return to;
+			},
+			writable: true,
+			configurable: true
+		});
+	}
+	
 	var precomputeCf7Select2Cache = function() {
 		
-		var cf7Select2Cache = [...cf7FieldsCache]; // shallow copy
+		var cf7Select2Cache = Object.assign([], cf7FieldsCache); // shallow copy
 		
 		jQuery.each(cf7Select2Cache, function(i, field) {
-			field = {...field}; // shallow copy
+			field = Object.assign({}, field); // shallow copy
 			field.lowerText = String(cf7Select2Cache[i].text).toLowerCase();
 			field['data-mailtags'] = false;
 			cf7Select2Cache[i] = field;
