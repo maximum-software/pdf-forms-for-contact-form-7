3c39ae2a6b072a4a8dd14bf4e817e9638bb29d7d
Alex <alex@maximum.guru>
2021-04-02 14:06:24 -0700

    Removed needless polyfill

--- js/frontend.js
+++ js/frontend.js
@@ -1,16 +1,3 @@
-// TODO: remove this?
-// Polyfill For IE 10+
-(function(){
-	if ( typeof window.CustomEvent === "function" ) return false;
-	function CustomEvent ( event, params ) {
-		params = params || { bubbles: false, cancelable: false, detail: null };
-		var evt = document.createEvent( 'CustomEvent' );
-		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-		return evt;
-	}
-	window.CustomEvent = CustomEvent;
-})();
-
 document.addEventListener( 'wpcf7mailsent', function( event )
 {
 	if(typeof event.detail !== 'object'
