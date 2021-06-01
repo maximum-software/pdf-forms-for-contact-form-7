8da1ef8949bbc6b3df1193cd12c12bc4c9b878cd
Alex <alex@maximum.guru>
2021-06-01 13:01:47 -0700

    Added 1.3.13 migration script

---
+++ migrations/1.3.13.php
@@ -0,0 +1,9 @@
+<?php
+	
+	if( ! class_exists('WPCF7') )
+		return;
+	
+	// if no Pdf.Ninja API version is set, set it to v1 to allow old forms to work in the same way as before
+	$version = WPCF7::get_option( 'wpcf7_pdf_forms_pdfninja_api_version' );
+	if( $version === null )
+		WPCF7::update_option( 'wpcf7_pdf_forms_pdfninja_api_version', 1 );
