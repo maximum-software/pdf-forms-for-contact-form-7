5fd691b4fd5f5cda7ff57546f360211ff75cd608
Alex <alex@maximum.guru>
2021-06-01 17:58:49 -0700

    Improved and enabled the migration scripts

--- pdf-forms-for-contact-form-7.php
+++ pdf-forms-for-contact-form-7.php
@@ -95,7 +95,7 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 			add_filter( 'cron_schedules', array( $this, 'cron_schedules' ) );
 			
 			// TODO: allow users to run this manually
-			//$this->upgrade_data();
+			add_action( 'admin_init', array( $this, 'upgrade_data' ) );
 		}
 		
 		/*
@@ -192,15 +192,16 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 		/*
 		 * Runs after plugin updates and triggers data migration
 		 */
-		public function upgrader_process_complete( $upgrader_object, $options )
+		public function upgrader_process_complete( $upgrader, $hook_extra )
 		{
 			$plugin_path = plugin_basename( __FILE__ );
 			
-			if( $options['action'] == 'update'
-			&& $options['type'] == 'plugin'
-			&& isset( $options['plugins'] ) )
+			if( $hook_extra['action'] == 'update'
+			&& $hook_extra['type'] == 'plugin'
+			&& isset( $hook_extra['plugins'] )
+			&& is_array( $hook_extra['plugins'] ) )
 			{
-				foreach( $options['plugins'] as $plugin )
+				foreach( $hook_extra['plugins'] as $plugin )
 				{
 					if( $plugin == $plugin_path )
 					{
@@ -243,6 +244,9 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 		 */
 		private function upgrade_data()
 		{
+			if( wp_doing_ajax() )
+				return;
+			
 			$old_version = get_transient( 'wpcf7_pdf_forms_updated_old_version' );
 			if( !$old_version )
 				return;
@@ -265,7 +269,8 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 		 */
 		private function run_data_migration( $script )
 		{
-			include( $script );
+			try { include( $script ); }
+			catch( Exception $e ) { }
 		}
 		
 		/*
