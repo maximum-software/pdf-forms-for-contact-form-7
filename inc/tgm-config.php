7ea17e636cdffacd2188301a2cda823e68c43629
Alex <alex@maximum.guru>
2022-02-10 19:45:28 -0800

    Improved tgm-config.php

--- inc/tgm-config.php
+++ inc/tgm-config.php
@@ -7,24 +7,21 @@
 	{
 		$plugins = array(
 			array(
-				'name'     => 'Contact Form 7',
-				'slug'     => 'contact-form-7',
-				'required' => true,
-				'version'  => '5.0',
+				'name'     => 'Contact Form 7',  // The plugin name.
+				'slug'     => 'contact-form-7',  // The plugin slug (typically the folder name).
+				'required' => true,              // If false, the plugin is only 'recommended' instead of required.
+				'version'  => '5.0',             // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			)
 		);
 		
 		$config = array(
-			'id'           => 'pdf-forms-for-contact-form-7',       // Unique ID for hashing notices for multiple instances of TGMPA.
-			'default_path' => '',                      // Default absolute path to bundled plugins.
-			'menu'         => 'tgmpa-install-plugins', // Menu slug.
-			'parent_slug'  => 'plugins.php',           // Parent menu slug.
-			'capability'   => 'manage_options',        // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-			'has_notices'  => true,                    // Show admin notices or not.
-			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-			'message'      => '',                      // Message to output right before the plugins table.
+			'id'           => 'pdf-forms-for-contact-form-7',  // Unique ID for hashing notices for multiple instances of TGMPA.
+			'menu'         => 'tgmpa-install-plugins',         // Menu slug.
+			'parent_slug'  => 'plugins.php',                   // Parent menu slug.
+			'capability'   => 'manage_options',                // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+			'has_notices'  => true,                            // Show admin notices or not.
+			'dismissable'  => true,                            // If false, a user cannot dismiss the nag message.
+			'is_automatic' => false,                           // Automatically activate plugins after installation or not.
 		);
 		
 		tgmpa( $plugins, $config );
