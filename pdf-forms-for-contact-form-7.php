a621589e73077f86356c5599ecb526dfeefeb491
Alex <alex@maximum.guru>
2022-07-21 17:34:36 -0700

    Accept all CF7 version revisions

--- pdf-forms-for-contact-form-7.php
+++ pdf-forms-for-contact-form-7.php
@@ -22,7 +22,7 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 	{
 		const VERSION = '2.0.6';
 		const MIN_WPCF7_VERSION = '5.0';
-		const MAX_WPCF7_VERSION = '5.6';
+		const MAX_WPCF7_VERSION = '5.6.99';
 		private static $BLACKLISTED_WPCF7_VERSIONS = array();
 		
 		private static $instance = null;
