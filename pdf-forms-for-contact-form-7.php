8b44ae60328b5fc44396755c3b11aa4ea3f3ee5d
Alex <alex@maximum.guru>
2023-04-03 13:51:03 -0700

    Minor refactor

--- pdf-forms-for-contact-form-7.php
+++ pdf-forms-for-contact-form-7.php
@@ -313,31 +313,30 @@ if( ! class_exists( 'WPCF7_Pdf_Forms' ) )
 		 */
 		public function admin_notices()
 		{
-			if( ( ! class_exists('WPCF7') || ! defined( 'WPCF7_VERSION' ) ) && current_user_can( 'install_plugins' ) && current_user_can( 'activate_plugins' ) )
+			if( ( ! class_exists('WPCF7') || ! defined( 'WPCF7_VERSION' ) ) )
 			{
-				echo WPCF7_Pdf_Forms::render_error_notice( 'cf7-not-installed', array(
-					'label' => esc_html__( "PDF Forms Filler for CF7 plugin error", 'pdf-forms-for-contact-form-7' ),
-					'message' => esc_html__( "The required plugin 'Contact Form 7' version is not installed!", 'pdf-forms-for-contact-form-7' ),
-				) );
+				if( current_user_can( 'install_plugins' ) && current_user_can( 'activate_plugins' ) )
+					echo WPCF7_Pdf_Forms::render_error_notice( 'cf7-not-installed', array(
+						'label' => esc_html__( "PDF Forms Filler for CF7 plugin error", 'pdf-forms-for-contact-form-7' ),
+						'message' => esc_html__( "The required plugin 'Contact Form 7' version is not installed!", 'pdf-forms-for-contact-form-7' ),
+					) );
 				return;
 			}
 			
-			if( ( ! defined( 'WPCF7_VERSION' ) || ! $this->is_wpcf7_version_supported( WPCF7_VERSION ) ) && current_user_can( 'update_plugins' ) )
-				echo WPCF7_Pdf_Forms::render_warning_notice( 'unsupported-cf7-version-'.WPCF7_VERSION, array(
-							'label'   => esc_html__( "PDF Forms Filler for CF7 plugin warning", 'pdf-forms-for-contact-form-7' ),
-							'message' =>
-								self::replace_tags(
-									esc_html__( "The currently installed version of 'Contact Form 7' plugin ({current-wpcf7-version}) may not be supported by the current version of 'PDF Forms Filler for CF7' plugin ({current-plugin-version}), please switch to 'Contact Form 7' plugin version {supported-wpcf7-version} or below to ensure that 'PDF Forms Filler for CF7' plugin would work correctly.", 'pdf-forms-for-contact-form-7' ),
-									array(
-										'current-wpcf7-version' => esc_html( defined( 'WPCF7_VERSION' ) ? WPCF7_VERSION : __( "Unknown version", 'pdf-forms-for-contact-form-7' ) ),
-										'current-plugin-version' => esc_html( self::VERSION ),
-										'supported-wpcf7-version' => esc_html( self::MAX_WPCF7_VERSION ),
-									)
-								),
-						) );
-			
-			if( ! class_exists('WPCF7') )
-				return;
+			if( ! $this->is_wpcf7_version_supported( WPCF7_VERSION ) )
+				if( current_user_can( 'update_plugins' ) )
+					echo WPCF7_Pdf_Forms::render_warning_notice( 'unsupported-cf7-version-'.WPCF7_VERSION, array(
+								'label'   => esc_html__( "PDF Forms Filler for CF7 plugin warning", 'pdf-forms-for-contact-form-7' ),
+								'message' =>
+									self::replace_tags(
+										esc_html__( "The currently installed version of 'Contact Form 7' plugin ({current-wpcf7-version}) may not be supported by the current version of 'PDF Forms Filler for CF7' plugin ({current-plugin-version}), please switch to 'Contact Form 7' plugin version {supported-wpcf7-version} or below to ensure that 'PDF Forms Filler for CF7' plugin would work correctly.", 'pdf-forms-for-contact-form-7' ),
+										array(
+											'current-wpcf7-version' => esc_html( defined( 'WPCF7_VERSION' ) ? WPCF7_VERSION : __( "Unknown version", 'pdf-forms-for-contact-form-7' ) ),
+											'current-plugin-version' => esc_html( self::VERSION ),
+											'supported-wpcf7-version' => esc_html( self::MAX_WPCF7_VERSION ),
+										)
+									),
+							) );
 			
 			if( ( $service = $this->get_service() ) )
 				$service->admin_notices();
