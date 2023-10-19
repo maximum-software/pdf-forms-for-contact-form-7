3a892ef7c06c6cd1439ae0fdaef7129c99495293
Alex <alex@maximum.guru>
2023-10-19 00:25:28 -0700

    Fixed an issue with UTF-8 not being base64-decoded properly

--- js/admin.js
+++ js/admin.js
@@ -87,9 +87,15 @@ jQuery(document).ready(function($) {
 		};
 	}
 	
+	var utf8atob = function(str)
+	{
+		// see https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
+		return (new TextDecoder()).decode(Uint8Array.from(atob(str), c => c.charCodeAt(0)));
+	};
+	
 	var base64urldecode = function(data)
 	{
-		return window.atob(strtr(data, {'.': '+', '_': '/'}).padEnd(data.length % 4, '='));
+		return utf8atob(strtr(data, {'.': '+', '_': '/'}).padEnd(data.length % 4, '='));
 	}
 	
 	var getTags = function(attachments, all) {
